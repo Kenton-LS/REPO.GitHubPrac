3d86e6bc46f30a1ba2910d79e4a41dae563e1854
Kenton <kentonlesliesmith@gmail.com>
2024-04-10 10:50:42 +0200

    Implemneted code for Player Name and Dragon Name, alongside RTB updating.

--- GitHubPrac/Form1.cs
+++ GitHubPrac/Form1.cs
@@ -12,9 +12,21 @@ namespace GitHubPrac
 {
     public partial class Form1 : Form
     {
+        string playerName;
+        string dragonName;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            playerName = txtPlayerName.Text;
+            dragonName = txtDragonName.Text;
+
+            rtbLog.Text = "Player Name: " + playerName +
+                            "\nDragon Name: " + dragonName;
         }
     }
 }
